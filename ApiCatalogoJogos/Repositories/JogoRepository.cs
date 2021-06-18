79161209c12838079814e583f216e6056385c79e
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-06-18 16:44:37 -0300

    commit

--- ApiCatalogoJogos/Repositories/JogoRepository.cs
+++ ApiCatalogoJogos/Repositories/JogoRepository.cs
@@ -13,17 +13,18 @@ namespace ApiCatalogoJogos.Repositories
             {Guid.Parse("698dc19d489c4e4db73e28a713eab07b"), new Jogo{ id = Guid.Parse("698dc19d489c4e4db73e28a713eab07b"), Nome = "Fifa 21", Produtora = "EA", Preco = 200 } },
             {Guid.Parse("74b87337454200d4d33f80c4663dc5e5"), new Jogo{ id = Guid.Parse("74b87337454200d4d33f80c4663dc5e5"), Nome = "Fifa 20", Produtora = "EA", Preco = 90 } },
             {Guid.Parse("b26986ceee60f744534aaab928cc12df"), new Jogo{ id = Guid.Parse("b26986ceee60f744534aaab928cc12df"), Nome = "Fifa 19", Produtora = "EA", Preco = 100 } },
-        }
-
+        };
 
         public Task Atualizar(Guid id, Jogo jogo)
         {
-            throw new NotImplementedException();
+            jogos[jogo.id] = jogo;
+            return Task.CompletedTask;
         }
 
         public Task Inserir(Jogo jogo)
         {
-            throw new NotImplementedException();
+            jogos.Add(jogo.id, jogo);
+            return Task.CompletedTask;
         }
 
         public Task<List<Jogo>> Obter(int pagina, int quantidade)
@@ -46,11 +47,12 @@ namespace ApiCatalogoJogos.Repositories
 
         public Task Remover(Guid id)
         {
-            throw new NotImplementedException();
+            jogos.Remove(id);
+            return Task.CompletedTask;
         }
         public void Dispose()
         {
-            throw new NotImplementedException();
+          // throw new NotImplementedException();
         }
     }
 }
